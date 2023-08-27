d5bfcea9bc79fc160c498ca4206740e5a14c7692
Deepanshu <deepanshukushwaha166@gmail.com>
2023-08-27 10:30:26 +0530

    initial

---
+++ api/server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import OpenAI from "openai";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "This is ChatGPT AI APP",
+  });
+});
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  
+});
+
+// console.log(apiKey);
+
+app.post("/", async (req, res) => {
+  try {
+
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: req.body.input }],
+      model: 'gpt-3.5-turbo',
+      temperature: 1,
+      max_tokens: 4000,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    console.log("PASSED: ", req.body.input);
+    console.log(completion.choices[0].message);
+    res.status(200).send({
+      bot: completion.choices[0].message,
+      // bot: response.data.choices[0].text
+    });
+  } catch (error) {
+    console.log("FAILED:", req.body.input);
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
+
+app.listen(4000, () => console.log("Server is running on port 4000"));
---
+++ client/src/App.jsx
@@ -0,0 +1,140 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import send from "./assets/send.svg";
+import user from "./assets/user.png";
+import bot from "./assets/bot.png";
+import loaderIcon from "./assets/loader.svg";
+import axios from "axios";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    document.querySelector(".layout").scrollTop =
+      document.querySelector(".layout").scrollHeight;
+  }, [posts]);
+
+  const fetchBotResponse = async () => {
+    const { data } = await axios.post(
+      "http://localhost:4000",
+      // "http://localhost:5173",
+      
+      { input },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+      );
+      // console.log(data);
+    return data;
+  };
+
+  const autoTypingBotResponse = (text) => {
+    let index = 0;
+    let interval = setInterval(() => {
+      if (index < text.length) {
+        setPosts((prevState) => {
+          let lastItem = prevState.pop();
+          if (lastItem.type !== "bot") {
+            prevState.push({
+              type: "bot",
+              post: text.charAt(index - 1),
+            });
+          } else {
+            prevState.push({
+              type: "bot",
+              post: lastItem.post + text.charAt(index - 1),
+            });
+          }
+          return [...prevState];
+        });
+        index++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 20);
+  };
+
+  const onSubmit = () => {
+    if (input.trim() === "") return;
+    updatePosts(input);
+    updatePosts("loading...", false, true);
+    setInput("");
+    fetchBotResponse().then((res) => {
+      console.log(res);
+      updatePosts(res.bot.content, true);
+    });
+  };
+
+  const updatePosts = (post, isBot, isLoading) => {
+    if (isBot) {
+      autoTypingBotResponse(post);
+    } else {
+      setPosts((prevState) => {
+        return [
+          ...prevState,
+          {
+            type: isLoading ? "loading" : "user",
+            post,
+          },
+        ];
+      });
+    }
+  };
+
+  const onKeyUp = (e) => {
+    if (e.key === "Enter" || e.which === 13) {
+      onSubmit();
+    }
+  };
+
+  return (
+    <main className="chatGPT-app">
+      <section className="chat-container">
+        <div className="layout">
+          {posts.map((post, index) => (
+            <div
+              key={index}
+              className={`chat-bubble ${
+                post.type === "bot" || post.type === "loading" ? "bot" : ""
+              }`}
+            >
+              <div className="avatar">
+                <img
+                  src={
+                    post.type === "bot" || post.type === "loading" ? bot : user
+                  }
+                />
+              </div>
+              {post.type === "loading" ? (
+                <div className="loader">
+                  <img src={loaderIcon} />
+                </div>
+              ) : (
+                <div className="post">{post.post}</div>
+              )}
+            </div>
+          ))}
+        </div>
+      </section>
+      <footer>
+        <input
+          className="composebar"
+          value={input}
+          autoFocus
+          type="text"
+          placeholder="Ask anything!"
+          onChange={(e) => setInput(e.target.value)}
+          onKeyUp={onKeyUp}
+        />
+        <div className="send-button" onClick={onSubmit}>
+          <img src={send} />
+        </div>
+      </footer>
+    </main>
+  );
+}
+
+export default App;
