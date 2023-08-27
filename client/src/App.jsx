064de5e5c2e15f381287e99cc1b77d67d33fc6c6
Deepanshu <deepanshukushwaha166@gmail.com>
2023-08-27 11:31:55 +0530

    url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   const fetchBotResponse = async () => {
     const { data } = await axios.post(
-      "http://localhost:4000",
+      "https://chat-g-pt-open-ai-ff9t.vercel.app",
       // "http://localhost:5173",
       
       { input },
